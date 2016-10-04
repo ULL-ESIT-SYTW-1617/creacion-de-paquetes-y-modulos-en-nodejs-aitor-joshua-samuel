191b3b544bc933a2646b7288ed72cd698d43cf76
Aitor Bernal <alu0100830064@ull.edu.es>
2016-10-04 16:40:16 +0100

    Añadiendo opcion por defecto

--- bin/gitbook-start.js
+++ bin/gitbook-start.js
@@ -1,7 +1,11 @@
 var argv = require('minimist')(process.argv.slice(2));
 console.dir(argv.x);
 
+if(JSON.stringify(argv) == '{"_":[]}'){
+ console.log("Añada un comando correcto");
+}
+
 if(argv.i)
 {
-  console.log("hola")
+  console.log("hola");
 }
