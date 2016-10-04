fbad922e912f3b2a9f2d5c64f568e06089b127d1
Joshua Pérez García <alu0100818819@ull.edu.es>
2016-10-04 17:09:49 +0100

    creando script para crear directorio

--- bin/gitbook-start.js
+++ bin/gitbook-start.js
@@ -1,8 +1,14 @@
 var argv = require('minimist')(process.argv.slice(2));
+var fs = require('fs-extended');
 console.dir(argv.x);
 
 if(JSON.stringify(argv) == '{"_":[]}'){
  console.log("Añada un comando correcto");
+}
+if(argv.n){
+	fs.createDir("./" + argv.n, function(err){
+		console.log(err);
+	})
 }
 
 if(argv.i)
